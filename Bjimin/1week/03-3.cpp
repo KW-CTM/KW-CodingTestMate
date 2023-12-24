6bc5d0627f2d5300805e74a3e27a583badea4921
root <root@DESKTOP-H81EL8G>
2023-12-24 12:19:13 +0900

    feat: 03-3 [11659] 구간 합 구하기

--- Bjimin/1week/03-3.cpp
+++ Bjimin/1week/03-3.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 
 int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	int N, M, i, l, h, j, temp, sum=0;
 	cin >> N >> M;
 	vector<int> sums;
@@ -17,10 +22,8 @@ int main() {
 	for(i=0; i<M; i++) {
 		sum = 0;
 		cin>>l>>h;
-		
-		sum = sums.at(h) - sums.at(l-1);
 
-		cout<<sum<<endl;
+		cout<<sums.at(h) - sums.at(l-1)<<"\n";
 	}
 
 }

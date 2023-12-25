8f6362d77b6ab6f6cce9665462c3c1c1f70bfeaf
ohyeong <oueong@naver.com>
2023-12-26 02:45:20 +0900

    [11660번] 구간합 구하기2

--- ohyeong/1week/4.cpp
+++ ohyeong/1week/4.cpp
@@ -12,22 +12,15 @@ int main(){
 
     cin >> cnt >> repeat;
 
+    for(int i=0; i<=cnt; i++){
+        array[i][0] = 0;
+        array[0][i] = 0;
+    }
+
     for(int i=1; i<=cnt; i++){
         for(int j=1; j<=cnt; j++){
             cin >> num;
-
-            if(i-1 > 0 && j-1 > 0){
-                array[i][j] = num + array[i-1][j] + array[i][j-1] - array[i-1][j-1];
-            }
-            else if(i-1 > 0){
-                array[i][j] = num + array[i-1][j];
-            }
-            else if(j-1 > 0){
-                array[i][j] = num + array[i][j-1];
-            }
-            else{
-                array[i][j] = num;
-            }
+            array[i][j] = num + array[i-1][j] + array[i][j-1] - array[i-1][j-1];
         }
     }
 
@@ -35,18 +28,6 @@ int main(){
         int firstx, firsty, endx, endy;
         cin >> firstx >> firsty;
         cin >> endx >> endy;
-        
-        if(firstx-1 > 0 && firsty-1 > 0){
-            cout << array[endx][endy] - array[firstx-1][endy] - array[endx][firsty-1] + array[firstx-1][firsty-1] <<"\n";
-        }
-        else if(firstx-1 > 0){
-            cout << array[endx][endy] - array[firstx-1][firsty]<<"\n";
-        }
-        else if(firsty-1 > 0){
-            cout << array[endx][endy] - array[firstx][firsty-1]<<"\n";
-        }
-        else{
-            cout << array[endx][endy]<<"\n";
-        }
+        cout << array[endx][endy] - array[firstx-1][endy] - array[endx][firsty-1] + array[firstx-1][firsty-1] <<"\n";
     }
 }

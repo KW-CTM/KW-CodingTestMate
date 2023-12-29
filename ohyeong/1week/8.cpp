6a9661953d35db21ca530092a8589624c4e04fe4
ohyeong <oueong@naver.com>
2023-12-30 03:06:35 +0900

    [1253번] 좋은수 구하기

---
+++ ohyeong/1week/8.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int cnt, start, end, num, result = 0;
+    cin >> cnt;
+    vector <int> array(cnt);
+
+    for(int i=0; i<cnt; i++){
+        cin >> array[i];
+    }
+
+    sort(array.begin(), array.end());
+
+    for(int i=0; i<cnt; i++){
+        start = 0;
+        end = cnt-1;
+        num = i;
+        
+        while(start < end){
+            int sum = array[start] + array[end];
+            
+            if(sum < array[num] || start == num){
+                start += 1;
+            }
+            else if(sum > array[num] || end == num){
+                end -= 1;
+            }
+            else if(sum == array[num]){
+                result += 1;
+                break;
+            }
+        }
+    }
+
+    cout << result << "\n";
+}

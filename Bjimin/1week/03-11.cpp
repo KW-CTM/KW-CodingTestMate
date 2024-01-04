53c273f67eed4fb18b51c432cabf7c5837547511
Bjimin <bjimin7@naver.com>
2024-01-05 02:54:14 +0900

    feat: 03-11 [1874] 스택으로 수열 만들기 풀이 주석 추가

--- Bjimin/1week/03-11.cpp
+++ Bjimin/1week/03-11.cpp
@@ -44,3 +44,81 @@ int main() {
     }
 
 }
+
+/*
+4 입력
+스택 empty
+push 1
+
+not empty
+top != 4
+push 2
+
+not empty
+top != 4
+push 3
+
+not empty
+top != 4
+push 4
+
+not empty
+top == 4
+pop
+
+3 입력
+스택 not empty
+top == 3
+pop
+
+6 입력
+스택 not empty
+top != 6
+push 5
+
+not empty
+top != 6
+push 6
+
+not empty
+top == 6
+pop
+
+8 입력
+...
+
+
+예제 2
+empty
+push 1
+
+not empty
+top == 1
+pop
+
+empty
+push 2
+
+not empty
+top == 2
+pop
+
+empty
+push 3
+
+not empty
+top != 5
+push 4
+
+not empty
+top != 5
+push 5
+
+not empty
+top == 5
+pop
+
+not empty
+top(4) != 3 (top > input) -> NO
+
+*/

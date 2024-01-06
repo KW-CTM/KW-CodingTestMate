2ab8e6b1a9d4985c4de7d78f6c06dc9d296daef1
Bjimin <bjimin7@naver.com>
2024-01-07 05:41:33 +0900

    feat: 03-5 [10986] 나머지 합 구하기

--- Bjimin/1week/03-5.cpp
+++ Bjimin/1week/03-5.cpp
@@ -10,27 +10,26 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	vector<int> remains;
-	remains.push_back(0);
-
-	int N, M, i, j, temp, count=0;
+	int N, M, i, temp;
+	long count=0;
 	cin>>N>>M;
 
-	int *arr = new int[M];
-	for(i=0; i<M; i++){
-		arr[i] = 0; //initialize
-	}
+	//***long으로 안했더니 틀렸음*** 
+
+	vector<long> remains(N+1, 0); //이건 변경안해도 상관없었음
+	vector<long> arr(M, 0); //자동으로 0으로 초기화
 
 	for(i=1; i<=N; i++){
 		cin>>temp;
 
-		remains.push_back(temp + remains[i-1]);
+		remains[i] = (temp + remains[i-1]);
 	}
 
 	for(i=1; i<=N; i++) {
-		remains[i] = remains[i]%M;
-		if(remains[i] == 0) { count++; }
-		arr[remains[i]]++;
+		temp = remains[i] % M;
+		//remains[i] = temp; 그냥 넣어도 괜찮음 but 불필요
+		if(temp == 0) { count++; }
+		arr[temp]++;
 	}
 
 	for(i=0; i<M; i++){
@@ -41,9 +40,11 @@ int main() {
 		r이 2로 고정되면 n * n-1 / 2
 		*/
 
-		count += arr[i] * (arr[i]-1) / 2;
+		//arr[i]가 1개이거나 0개일 경우는? -> 틀릴 것이라고 예상
+
+		if(arr[i] > 1) 
+			count += (arr[i] * (arr[i]-1)) / 2; //무조건 짝수일 것이므로 2로 나누는 것은 상관없음
 	}
 
 	cout<<count<<endl;
-	delete[] arr;
 }

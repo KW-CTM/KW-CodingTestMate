c37ad1ab7125a5aea4a92c14e23e30d05c33f766
root <root@DESKTOP-H81EL8G>
2024-01-07 04:59:44 +0900

    feat: 03-5 [10986] 나머지 합 구하기 [시간초과]

--- Bjimin/1week/03-5.cpp
+++ Bjimin/1week/03-5.cpp
@@ -2,23 +2,53 @@
 #include <vector>
 using namespace std;
 
+int combination(int n, int r);
+
 int main() {
 
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	vector<int> sums;
-	sums.push_back(0);
+	vector<int> remains;
+	remains.push_back(0);
 
 	int N, M, i, j, temp, count=0;
 	cin>>N>>M;
 
-	for(i=0; i<N; i++){
-		cin>>temp;
-		sums.push_back(sums.at(i-1) + temp);
+	int *arr = new int[M];
+	for(i=0; i<M; i++){
+		arr[i] = 0; //initialize
 	}
 
+	for(i=1; i<=N; i++){
+		cin>>temp;
+		remains.push_back((temp + remains[i-1])%M);
+		if(remains[i] == 0) { count++; }
+		arr[remains[i]]++;
+	}
 
-	cout<<count<<endl;	
+	for(i=0; i<M; i++){
+		//나머지값들 등장한 횟수
+		//조합 계산
+		count += combination(arr[i], 2);
+	}
+
+	cout<<count<<endl;
+	delete[] arr;
+}
+
+int combination(int n, int r) {
+	if (n <= 0 || r <= 0) {
+		if(r == 0) return 1;
+		else return 0;
+	} 
+	
+	if(n == r) {
+		return 1;
+	} else if (n < r) {
+		return 0;
+	}
+
+	return combination(n-1, r-1) + combination(n-1, r);
 }
